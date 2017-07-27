55168764cef4a2808a0cc69bd8028f699bf88924
Steve Kuo <skuo314@gmail.com>
2017-07-27 19:08:14 -0400

    can view drug alternatives on concept click

--- app/redux/reducers/rootReducer.jsx
+++ app/redux/reducers/rootReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import conceptsReducer from './conceptsReducer';
+import alternativesReducer from './alternativesReducer';
 
 const rootReducer = combineReducers({
-	conceptsReducer
+	conceptsReducer,
+	alternativesReducer
 });
 
 export default rootReducer;
--- app/redux/utils/WebAPIUtils.jsx
+++ app/redux/utils/WebAPIUtils.jsx
@@ -8,3 +8,21 @@ export const fetchConcept = drug => {
 		})
 		return request;
 };
+
+export const searchDrugGroup = drug => {
+	const url = `https://rxnav.nlm.nih.gov/REST/rxcui/${drug.rxcui}/related.json?tty=IN`;
+	const request = axios.get(url)
+		.then(response => {
+			return response;
+		})
+		return request;
+};
+
+export const searchAlternativeDrugs = rxcui => {
+	const url = `https://rxnav.nlm.nih.gov/REST/rxcui/${rxcui}/related.json?tty=SCD+SBD`;
+	const request = axios.get(url)
+		.then(response => {
+			return response;
+		})
+		return request;
+};
